2de344d5b9e96568778852d37d6556652eed76fa
mitchel noble <mitchelparkernoble@gmail.com>
2024-03-29 16:36:17 -0700

    completed drawing vector lines

--- main.js
+++ main.js
@@ -1,25 +1,45 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75,
-  window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth,
-  window.innerHeight);
-document.body.appendChild(renderer.domElement);
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xaaffbb });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
 
-camera.position.z = 5;
+const scene = new THREE.Scene();
+  
+
+//creating a blue LineBasicMaterial
+const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
+
+//creating the vectors
+const points = [];
+points.push(new THREE.Vector3(-10,0,0));
+points.push(new THREE.Vector3(0,10,0));
+points.push(new THREE.Vector3(10, 0, 0));
+points.push(new THREE.Vector3(-20,0,0));
+points.push(new THREE.Vector3(0,20,0));
+points.push(new THREE.Vector3(20, 0, 0));
+points.push(new THREE.Vector3(-20,0,0));
+points.push(new THREE.Vector3(0,20,0));
+points.push(new THREE.Vector3(20, 0, 0));
+
+//geometry takes the lines from the object vectors
+const geometry = new
+  THREE.BufferGeometry().setFromPoints(points);
+
+//connecting the lines with the material
+const line = new THREE.Line(geometry, material);
+
+scene.add(line);
+renderer.render(scene, camera);
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.04;
-  cube.rotation.y += 0.02;
+  line.rotation.x += 0.01;
+  line.rotation.y += 0.08;
   renderer.render(scene, camera);
 }
 animate();
